e0194ac31e40a06e885ac5c71f551b1dfe3b8d18
René Heidrich <rene.heidrich-privat@outlook.de>
2020-05-03 11:41:57 +0200

    Created AppProjects solution and 5 projects

---
+++ Application.Views/Source/Application.ViewModels/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Application.ViewModels
+{
+    public class Class1
+    {
+    }
+}
---
+++ Application.Views/Source/Framework.Diagnostics/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Framework.Diagnostics
+{
+    public class Class1
+    {
+    }
+}
